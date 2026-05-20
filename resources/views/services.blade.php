da9024b2c8031af112b91e40b54cd403ffff3894
Syauki23 <ahmadsyauki23@gmail.com>
2026-05-20 19:03:10 +0700

    Tahun perusahaan diubah menjadi “Since 2017”.

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -129,7 +129,7 @@
       <div class="stat-item">
         <div class="stat-icon"><svg viewBox="0 0 24 24" width="32" height="32" stroke="var(--navy-mid)" stroke-width="1.5" fill="none"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg></div>
         <div class="stat-text">
-          <h4>2017-2025</h4>
+          <h4>Since 2017</h4>
           <p>Maritime Assessment &<br>Consulting Experience</p>
         </div>
       </div>
